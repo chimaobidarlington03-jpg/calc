a2c6dc171b46c2061fce827e8c81fde23c05d7b0
chimaobidarlington03-jpg <chimaobidarlington03@gmail.com>
2026-03-18 20:20:30 +0100

    Update product.js

--- product.js
+++ product.js
@@ -3,8 +3,8 @@ const products = [
         name: "YAMAHA",
         price: 15000,
         img: "https://i.ibb.co/1j3yBBV/20260312-135740.jpg"
-    },
-    {
+    }
+    
         name: "T-Shirt",
         price: 5000,
         img: "https://i.ibb.co/1j3yBBV/20260312-135740.jpg"
